955680852cd99b3ff12108e372e21c47be4929c2
Abin S <bm20mtech11005@iith.ac.in>
2023-04-02 15:10:42 +0530

    change in local feature-branch-2

--- sample2.py
+++ sample2.py
@@ -2,6 +2,9 @@
 
 print("created in local at feature-branch-1")
 
-print("added new statement at remote on feature-branch-1" )
+print("added new statement at remote on feature-branch-1")
 
-print("second new statement at remote on feature-branch-2")
+print("second new statement at remote on feature-branch-1")
+
+print('added statement to feature-branch-2')
+print("I odon't want this in featrue branch-1")
