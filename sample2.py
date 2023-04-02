0a431d97774b1b450ae379c5e2f7919bcd461946
Abin S <bm20mtech11005@iith.ac.in>
2023-04-02 15:16:10 +0530

    added some changes in feature-branch-1

--- sample2.py
+++ sample2.py
@@ -7,4 +7,8 @@ print("added new statement at remote on feature-branch-1")
 print("second new statement at remote on feature-branch-1")
 
 print('added statement to feature-branch-2')
-print("I odon't want this in featrue branch-1")
+print("I don't want this in featrue branch-1")
+
+print(
+    'added some changes in feature-branch-1'
+)
