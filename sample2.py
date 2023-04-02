f990d1871771fad9993e822efaf638ca2263176f
abin102 <129642512+abin102@users.noreply.github.com>
2023-04-02 16:50:01 +0530

    Update sample2.py

--- sample2.py
+++ sample2.py
@@ -9,9 +9,6 @@ print("second new statement at remote on feature-branch-1")
 print('added statement to feature-branch-2')
 print("I don't want this in featrue branch-1")
 
-print(
-    'added some changes in feature-branch-1'
-)
 
 print("I want this in feature-branch-2")
 
